a7ff40f91cfc19f7b82b08a5c54a6eefa76d2ad6
bibashmgr <bibashthapamagar21@gmail.com>
2021-06-25 15:53:57 +0545

    Day 05

---
+++ loops/while/app.js
@@ -0,0 +1,21 @@
+// While Loop:
+// Syntax:
+// while(condition){ // code to be executed }
+
+let x = 0;
+while (x < 10) {
+    // console.log(x);
+    x++;
+}
+
+// Do While Loop:
+// Syntax:
+// do{ // code to be executed }while(condition)
+
+let y = 0;
+do {
+    console.log(y);
+    y++;
+} while (y < 0)
+
+// even the condition isnot met,it will still execute the code
